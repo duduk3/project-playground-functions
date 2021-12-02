33c6774faef6ee9a7b0a4892ed632f2d57d7ebeb
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-12-02 19:52:23 -0300

    desafio 7 completo

--- src/challenges.js
+++ src/challenges.js
@@ -1,4 +1,6 @@
+//============================================
 //* Desafio 1
+//============================================
 function compareTrue(arg1, arg2) {
   if(arg1 === true && arg2 === true) {
     return true;
@@ -6,32 +8,41 @@ function compareTrue(arg1, arg2) {
   return false;
 }
 
+//============================================
 //* Desafio 2 
+//============================================
 function calcArea(base, height) {
   return base * height / 2
 }
 
 
+//============================================
 //* Desafio 3                               
+//============================================
 function splitSentence(frase) {
   let separatedFrase = [];
   return separatedFrase = frase.split(' ');
 }
 
 
+//============================================
 //* Desafio 4
+//============================================
 function concatName(args) {
   return  args[args.length -1] + ", " + args[0];
 }
 
 
+//============================================
 //* Desafio 5
+//============================================
 function footballPoints(wins, ties) {
   return wins * 3 + ties * 1;
 }
 
-
+//============================================
 //* Desafio 6
+//============================================
 function highestCount(values) {
   let num = 0;
   let count = 0;
@@ -48,11 +59,34 @@ function highestCount(values) {
   return count; 
 }
 
-
+//============================================
 //* Desafio 7
-function catAndMouse() {
-  // seu código aqui
+//============================================
+function catAndMouse(mouse, cat1, cat2) {
+  let cat1Win = mouse - cat1;
+  let cat2Win = mouse - cat2;
+  let trombada = 'os gatos trombam e o rato foge.'
+  if ( cat1Win < 0) {
+    cat1Win = cat1Win * -1;
+  }
+  if ( cat2Win < 0) {
+    cat2Win = cat2Win * -1;
+  }
+
+  if (cat1Win < cat2Win) {
+    trombada = 'cat1';
+  }
+  else if(cat1Win > cat2Win) {
+    trombada = 'cat2';
+  }
+  // else {
+  //   trombada
+  // }
+  return trombada;
 }
+
+console.log(catAndMouse(10, 8, 12));
+
 
 // Desafio 8
 function fizzBuzz() {
